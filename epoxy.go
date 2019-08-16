c11c9ff828b7ea831ab858dacb1f03e74e9b883c
Louise van der Lith <louisevanderlith@gmail.com>
2019-08-16 11:09:13 +0200

    Check for Template will give error. Clients now limited.

--- epoxy.go
+++ epoxy.go
@@ -235,10 +235,29 @@ func (e *Epoxy) GetRouter() http.Handler {
 	return e.router
 }
 
+func limitedClients(h http.Handler, max int) http.Handler {
+	sema := make(chan struct{}, max)
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		sema <- struct{}{}
+		defer func() { <-sema }()
+
+		h.ServeHTTP(w, r)
+	})
+}
+
 //Boot starts the Epoxy Objects to serve a configured service.
 func (e *Epoxy) Boot() error {
 	e.server = newServer(e.service.Port)
 	e.server.Handler = e.router
+
+	return e.server.ListenAndServe()
+}
+
+//Boot starts the Epoxy Objects to serve a configured service with a max client limit
+func (e *Epoxy) BootLimited(maxClients int) error {
+	e.server = newServer(e.service.Port)
+	e.server.Handler = limitedClients(e.router, maxClients)
 
 	return e.server.ListenAndServe()
 }
