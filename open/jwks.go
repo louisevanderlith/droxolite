79c0312360df8109602211f8d93bbc3d3edc3cb1
louisevanderlith <louisevanderlith@gmail.com>
2020-10-23 11:41:52 +0200

    Added middleware auth functions

---
+++ open/jwks.go
@@ -0,0 +1,53 @@
+package open
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"net/http"
+)
+
+type Jwks struct {
+	Keys []JSONWebKeys `json:"keys"`
+}
+
+type JSONWebKeys struct {
+	Kty string   `json:"kty"`
+	Kid string   `json:"kid"`
+	Use string   `json:"use"`
+	N   string   `json:"n"`
+	E   string   `json:"e"`
+	X5c []string `json:"x5c"`
+}
+
+func getPemCert(issUrl string, token *jwt.Token) (string, error) {
+	cert := ""
+	certUrl := fmt.Sprintf("%s/protocol/openid-connect/certs", issUrl)
+	resp, err := http.Get(certUrl)
+
+	if err != nil {
+		return cert, err
+	}
+	defer resp.Body.Close()
+
+	var jwks = Jwks{}
+	err = json.NewDecoder(resp.Body).Decode(&jwks)
+
+	if err != nil {
+		return cert, err
+	}
+
+	for k, _ := range jwks.Keys {
+		if token.Header["kid"] == jwks.Keys[k].Kid {
+			cert = "-----BEGIN CERTIFICATE-----\n" + jwks.Keys[k].X5c[0] + "\n-----END CERTIFICATE-----"
+		}
+	}
+
+	if cert == "" {
+		err := errors.New("unable to find appropriate key")
+		return cert, err
+	}
+
+	return cert, nil
+}
---
+++ open/login.go
@@ -0,0 +1,97 @@
+package open
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+	"github.com/coreos/go-oidc"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"net/http"
+	"time"
+)
+
+type uiprotector struct {
+	authConfig *oauth2.Config
+}
+
+func NewUILock(cfg *oauth2.Config) uiprotector {
+	return uiprotector{authConfig: cfg}
+}
+
+func (p uiprotector) Login(w http.ResponseWriter, r *http.Request) {
+	state := generateStateOauthCookie(w)
+	http.Redirect(w, r, p.authConfig.AuthCodeURL(state), http.StatusTemporaryRedirect)
+}
+
+func (p uiprotector) Callback(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+
+	state, err := r.Cookie("oauthstate")
+
+	if err != nil {
+		http.Error(w, "state not found", http.StatusInternalServerError)
+		return
+	}
+
+	if r.URL.Query().Get("state") != state.Value {
+		http.Error(w, "state did not match", http.StatusBadRequest)
+		return
+	}
+
+	oauth2Token, err := p.authConfig.Exchange(ctx, r.URL.Query().Get("code"))
+	if err != nil {
+		http.Error(w, "Failed to exchange token: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rawIDToken, ok := oauth2Token.Extra("id_token").(string)
+
+	if !ok {
+		http.Error(w, "No id_token field in oauth2 token.", http.StatusInternalServerError)
+		return
+	}
+
+	acccookie := http.Cookie{Name: "acctoken", Value: oauth2Token.AccessToken, Expires: oauth2Token.Expiry}
+	http.SetCookie(w, &acccookie)
+
+	idcookie := http.Cookie{Name: "idtoken", Value: rawIDToken, Expires: oauth2Token.Expiry}
+	http.SetCookie(w, &idcookie)
+
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+func generateStateOauthCookie(w http.ResponseWriter) string {
+	var expiration = time.Now().Add(365 * 24 * time.Hour)
+
+	b := make([]byte, 16)
+	rand.Read(b)
+	state := base64.URLEncoding.EncodeToString(b)
+	cookie := http.Cookie{Name: "oauthstate", Value: state, Expires: expiration}
+	http.SetCookie(w, &cookie)
+
+	return state
+}
+
+func LoginMiddleware(verifier *oidc.IDTokenVerifier, next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		rawIDToken, err := r.Cookie("idtoken")
+
+		if err != nil {
+			http.Redirect(w, r, "/login", http.StatusTemporaryRedirect)
+			return
+		}
+
+		idToken, err := verifier.Verify(context.Background(), rawIDToken.Value)
+		if err != nil {
+			http.Error(w, "Failed to verify ID Token: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		rawaccToken, _ := r.Cookie("acctoken")
+		xidn := context.WithValue(r.Context(), "accToken", rawaccToken.Value)
+
+		idn := context.WithValue(xidn, "idToken", idToken)
+
+		next.ServeHTTP(w, r.WithContext(idn))
+	}
+}
