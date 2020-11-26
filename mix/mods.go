ab7f729893768a39f3f8f12325e1c35413a6d2af
louisevanderlith <louisevanderlith@gmail.com>
2020-11-26 21:45:32 +0200

    Added User Claims to IdentityMod

--- mix/mods.go
+++ mix/mods.go
@@ -1,6 +1,7 @@
 package mix
 
 import (
+	"github.com/coreos/go-oidc"
 	"net/http"
 )
 
@@ -14,9 +15,31 @@ func EndpointMod(endpoints map[string]string) ModFunc {
 
 func IdentityMod(clientId string) ModFunc {
 	return func(f MixerFactory, r *http.Request) {
+		f.SetValue("ClientID", clientId)
+
 		tkn := r.Context().Value("Token")
 
-		f.SetValue("ClientID", clientId)
+		if tkn == nil {
+			return
+		}
+
 		f.SetValue("Token", tkn)
+
+		tknVal := r.Context().Value("IDToken")
+
+		if tknVal == nil {
+			return
+		}
+
+		idToken := tknVal.(*oidc.IDToken)
+		claims := make(map[string]interface{})
+		err := idToken.Claims(&claims)
+
+		if err != nil {
+			panic(err)
+			return
+		}
+
+		f.SetValue("User", claims)
 	}
 }
